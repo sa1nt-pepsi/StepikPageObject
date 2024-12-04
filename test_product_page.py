b0ed93076a2ad43bafda1778e5f083078570d7c5
sa1nt-pepsi <solve.et.coagula666@yandex.ru>
2024-12-04 18:35:03 +0300

    тесты добавления товара в корзину и сообщение об успешном добавлении

---
+++ test_product_page.py
@@ -0,0 +1,20 @@
+import time
+import pytest
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+
+
+def test_user_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.product_name_is_correct()
+    page.should_be_correct_adding_product_price()
+
+def test_user_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()  
